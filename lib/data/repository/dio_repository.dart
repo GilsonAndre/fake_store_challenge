b5a1471594875ed2b9386f2077437769634be7f9
GilsonAndre <gilsonandre1999@gmail.com>
2024-08-22 19:21:38 -0300

    category dio created

--- lib/data/repository/dio_repository.dart
+++ lib/data/repository/dio_repository.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:fake_store_one/data/models/category_model.dart';
 import 'package:fake_store_one/data/models/product_model.dart';
+import 'package:flutter/foundation.dart';
 
 class DioRepository {
   final Dio dio = Dio();
@@ -14,8 +16,22 @@ class DioRepository {
           (e) => ProductModel.fromJson(e),
         )
         .toList();
-    print(results);
+    print('PRODUCTS $results');
     print(results.length);
     return products;
+  }
+  Future<List<CategoryModel>> getCategory() async {
+    final response = await dio.get('https://api.escuelajs.co/api/v1/products');
+
+    List<dynamic> results = response.data;
+
+    final List<CategoryModel> category = results
+        .map(
+          (e) => CategoryModel.fromJson(e),
+        )
+        .toList();
+    print('CATEGORY $category');
+    print(results.length);
+    return category;
   }
 }
