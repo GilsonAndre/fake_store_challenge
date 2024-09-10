9fc4e8675cef37e87943667c7f05d6edae4221f8
GilsonAndre <gilsonandre1999@gmail.com>
2024-09-09 21:16:19 -0300

    widget Toast info created

--- lib/ui/widgets/toast_info.dart
+++ lib/ui/widgets/toast_info.dart
@@ -1,1 +1,16 @@
 //toast responsable to show a bar with text and button
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+toastInfo({
+  required String msg,
+}) {
+  return Fluttertoast.showToast(
+    msg: msg,
+    backgroundColor: Colors.black,
+    textColor: Colors.white,
+    gravity: ToastGravity.TOP,
+    fontSize: 16,
+    toastLength: Toast.LENGTH_SHORT,
+  );
+}
