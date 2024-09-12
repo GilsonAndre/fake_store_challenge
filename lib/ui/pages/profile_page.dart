19080c242f0c15833bb88010b14962a8a6afea5b
GilsonAndre <gilsonandre1999@gmail.com>
2024-09-12 20:42:59 -0300

    profile page with photo appbar

--- lib/ui/pages/profile_page.dart
+++ lib/ui/pages/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -8,6 +9,22 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Profile'),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              height: 80.h,
+              width: 80.w,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: NetworkImage(
+                      'https://www.racasdecachorro.com.br/wp-content/uploads/2019/04/683-golden-37260766.jpg'),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
